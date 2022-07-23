ed5cced80b5ec106c1bd189e5f0c43621c633e74
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-07-23 13:04:20 -0300

    test: ensure GetRefreshToken returns account

---
+++ src/infra/db/mongodb/refresh-token-repository/get-refresh-token.spec.ts
@@ -0,0 +1,48 @@
+import type { AccountModel } from "../../../../domain/models/account";
+import { mongoHelper } from "../helpers/mongo-helper";
+import { GetRefreshTokenMongoRepository } from "./get-refresh-token";
+
+const makeSut = () => {
+  const sut = new GetRefreshTokenMongoRepository();
+
+  return {
+    sut,
+  };
+};
+
+describe("GetRefreshToken", () => {
+  beforeAll(async () => {
+    await mongoHelper.connect();
+    const accountCollection = mongoHelper.getCollection("account");
+    await (
+      await accountCollection
+    ).insertOne({
+      email: "valid_mail@mail.com",
+      name: "valid_name",
+      password: "valid_password",
+      refreshToken: "1234",
+    } as AccountModel);
+  });
+
+  afterAll(async () => {
+    const accountCollection = await mongoHelper.getCollection("account");
+    await accountCollection.deleteMany({});
+    await mongoHelper.disconnect();
+  });
+
+  test("should return account", async () => {
+    const { sut } = makeSut();
+
+    const account = await sut.get("1234");
+
+    expect(account.id).toBeTruthy();
+    expect(account).toStrictEqual(
+      expect.objectContaining({
+        email: "valid_mail@mail.com",
+        name: "valid_name",
+        password: "valid_password",
+        refreshToken: "1234",
+      })
+    );
+  });
+});
