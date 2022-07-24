a0111282c43da2aa792de53e42a0f0985cef5f6d
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-07-23 23:08:09 -0300

    feat: add setupRoutes on app

---
+++ src/main/config/routes.ts
@@ -0,0 +1,13 @@
+import type { Express } from "express";
+import { Router } from "express";
+import fg from "fast-glob";
+
+export const setupRoutes = (app: Express) => {
+  const router = Router();
+
+  app.use("/api", router);
+
+  fg.sync("**/src/main/routes/**routes.ts").map(async (file) => {
+    (await import(`../../../${file}`)).default(router);
+  });
+};
