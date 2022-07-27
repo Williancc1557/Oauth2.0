844b00e37b8b9655e6f9551d365e8abcb813b837
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-07-26 22:04:21 -0300

    feat: add AddAccount protocol

---
+++ src/presentation/protocols/add-account.ts
@@ -0,0 +1,11 @@
+import type { AccountModel } from "../../domain/models/account";
+
+export interface AddAccountInput {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface AddAccount {
+  add: (account: AddAccountInput) => Promise<AccountModel>;
+}
