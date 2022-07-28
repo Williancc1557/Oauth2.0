8585d718e3ae16b85c63e734d99166f1b8e5dd43
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-07-28 00:36:59 -0300

    test: ensure AddAccountMongoRepository return account if success

---
+++ src/infra/db/mongodb/account-repository/add-account-repository.spec.ts
@@ -0,0 +1,52 @@
+import type { CreateRefreshToken } from "../../../../presentation/protocols/create-refresh-token";
+import { mongoHelper } from "../helpers/mongo-helper";
+import { AddAccountMongoRepository } from "./add-account-repository";
+
+const makeCreateRefreshTokenStub = () => {
+    class CreateRefreshTokenStub implements CreateRefreshToken {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        public create(userId: string): string {
+            return "valid_refresh_token";
+        }
+    }
+
+    return new CreateRefreshTokenStub();
+};
+
+const makeSut = () => {
+    const createRefreshTokenStub = makeCreateRefreshTokenStub();
+    const sut = new AddAccountMongoRepository(createRefreshTokenStub);
+
+    return {
+        sut,
+        createRefreshTokenStub,
+    };
+};
+
+describe("AddAccountMongoRepository", () => {
+    beforeAll(async () => {
+        await mongoHelper.connect();
+    });
+
+    afterAll(async () => {
+        await mongoHelper.disconnect();
+    });
+
+    test("should return account if sucess", async () => {
+        const { sut } = makeSut();
+
+        const req = await sut.add({
+            email: "valid_mail@mail.com",
+            name: "valid_name",
+            password: "valid_password",
+        });
+
+        expect(req.refreshToken).toBeTruthy();
+        expect(req.id).toBeTruthy();
+        expect(req).toStrictEqual(expect.objectContaining({
+            email: "valid_mail@mail.com",
+            name: "valid_name",
+            password: "valid_password",
+        }));
+    });
+});
