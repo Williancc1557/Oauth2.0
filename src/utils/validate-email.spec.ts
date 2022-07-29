33a5393e72dfbd0dc543c778ad9596e5fb804225
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-07-28 23:00:52 -0300

    test: check all returns of ValidateEmail

---
+++ src/utils/validate-email.spec.ts
@@ -0,0 +1,37 @@
+import { UtilValidateEmail } from "./validate-email";
+import validator from "validator";
+
+jest.mock("validator", () => ({
+    isEmail(): boolean {
+        return true;
+    },
+}));
+
+const makeSut = (): UtilValidateEmail => {
+    return new UtilValidateEmail();
+};
+
+describe("EmailValidator Adapter", () => {
+    test("should return false if validator returns false", () => {
+        const sut = makeSut();
+        jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
+        const validate = sut.validate("invalid_email@email.com");
+
+        expect(validate).toBe(false);
+    });
+
+    test("should return true if validator returns true", () => {
+        const sut = makeSut();
+        const validate = sut.validate("valid_email@email.com");
+
+        expect(validate).toBe(true);
+    });
+
+    test("should call validator with correct email", () => {
+        const sut = makeSut();
+        const isEmailSpy = jest.spyOn(validator, "isEmail");
+        sut.validate("any_email@email.com");
+
+        expect(isEmailSpy).toHaveBeenCalledWith("any_email@email.com");
+    });
+});
