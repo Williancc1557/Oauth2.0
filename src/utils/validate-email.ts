8abef15be199cbbf11c9de58a2ad605c0608b0ff
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-07-28 23:00:22 -0300

    feat: add ValidateEmail

---
+++ src/utils/validate-email.ts
@@ -0,0 +1,8 @@
+import type { ValidateEmail } from "../presentation/protocols/validate-email";
+import validator from "validator";
+
+export class UtilValidateEmail implements ValidateEmail {
+    public validate(email: string): boolean {
+        return validator.isEmail(email);
+    }
+}
