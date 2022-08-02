9ff6aab235a397a4ab51d01647429bf9c494d16f
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-08-01 21:15:41 -0300

    feat: ensure SignInController returns statusCode 400 if email is not provided

---
+++ src/presentation/controller/sign-in/sign-in.ts
@@ -0,0 +1,32 @@
+import { MissingParamError } from "../../erros/missing-param-error";
+import { badRequest, serverError } from "../../helpers/http-helper";
+import type { Controller } from "../../protocols/controller";
+import type { HttpRequest, HttpResponse } from "../../protocols/http";
+import type { PasswordValidator } from "../../protocols/password-validator";
+import type { RequiredParams } from "../../protocols/required-params";
+import type { ValidateEmail } from "../../protocols/validate-email";
+
+export class SignInController implements Controller {
+  public constructor(
+    private readonly validateEmail: ValidateEmail,
+    private readonly passwordValidator: PasswordValidator,
+    private readonly requiredParams: RequiredParams
+  ) {}
+
+  public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const requiredParam = this.requiredParams.check(
+        ["email", "password"],
+        httpRequest.body
+      );
+
+      if (requiredParam) {
+        return badRequest(new MissingParamError(requiredParam));
+      }
+
+      return null;
+    } catch (err) {
+      return serverError();
+    }
+  }
+}
