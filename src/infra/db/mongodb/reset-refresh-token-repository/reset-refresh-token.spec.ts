eec0c26a66ec2a8cee836e23e01f7d388c4d09d2
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-08-07 13:31:04 -0300

    test: ensure ResetRefreshTokenMongoRepository returns new refreshToken and update refreshToken of Database

---
+++ src/infra/db/mongodb/reset-refresh-token-repository/reset-refresh-token.spec.ts
@@ -0,0 +1,77 @@
+import { ObjectId } from "mongodb";
+import type { CreateRefreshToken } from "../../../../data/protocols/create-refresh-token";
+import type { AccountModel } from "../../../../domain/models/account";
+import { mongoHelper } from "../helpers/mongo-helper";
+import { ResetRefreshTokenMongoRepository } from "./reset-refresh-token-repository";
+
+const makeCreateRefreshTokenStub = () => {
+  class CreateRefreshTokenStub implements CreateRefreshToken {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    public create(userId: string): string {
+      return "new_refresh_token";
+    }
+  }
+
+  return new CreateRefreshTokenStub();
+};
+
+const makeSut = () => {
+  const createRefreshTokenStub = makeCreateRefreshTokenStub();
+  const sut = new ResetRefreshTokenMongoRepository(createRefreshTokenStub);
+
+  return {
+    sut,
+    createRefreshTokenStub,
+  };
+};
+
+describe("ResetRefreshTokenMongoRepository", () => {
+  let id: string;
+
+  beforeAll(async () => {
+    await mongoHelper.connect();
+    const accountCollection = mongoHelper.getCollection("account");
+    const { insertedId } = await (
+      await accountCollection
+    ).insertOne({
+      email: "valid_mail@mail.com",
+      name: "valid_name",
+      password: "valid_password",
+      refreshToken: "1234",
+    } as AccountModel);
+
+    id = String(insertedId);
+  });
+
+  afterAll(async () => {
+    const accountCollection = await mongoHelper.getCollection("account");
+    await accountCollection.deleteMany({});
+    await mongoHelper.disconnect();
+  });
+
+  test("should return refreshToken if success", async () => {
+    const { sut } = makeSut();
+
+    const res = await sut.reset(id);
+
+    const accountCollection = await mongoHelper.getCollection("account");
+    await accountCollection.findOne({
+      _id: new ObjectId(id),
+    });
+
+    expect(res).toBe("new_refresh_token");
+  });
+
+  test("should update account's refreshToken", async () => {
+    const { sut } = makeSut();
+
+    await sut.reset(id);
+
+    const accountCollection = await mongoHelper.getCollection("account");
+    const account = await accountCollection.findOne({
+      _id: new ObjectId(id),
+    });
+
+    expect(account.refreshToken).toBe("new_refresh_token");
+  });
+});
