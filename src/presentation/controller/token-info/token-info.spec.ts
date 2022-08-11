5bec10be140ecb7137ac0eaaf2c80c3b054f2493
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-08-11 09:33:11 -0300

    feat: ensure TokenInfoController returns statusCode 400 if refreshToken is not provided

---
+++ src/presentation/controller/token-info/token-info.spec.ts
@@ -0,0 +1,38 @@
+import type { RequiredParams } from "../../protocols";
+import { TokenInfoController } from "./token-info";
+
+const makeRequiredParamsStub = () => {
+  class RequiredParamsStub implements RequiredParams {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    public check(requiredParams: Array<string>, body: any): string {
+      return;
+    }
+  }
+
+  return new RequiredParamsStub();
+};
+
+const makeSut = () => {
+  const requiredParamsStub = makeRequiredParamsStub();
+  const sut = new TokenInfoController(requiredParamsStub);
+  return {
+    sut,
+    requiredParamsStub,
+  };
+};
+
+describe("TokenInfo controller", () => {
+  test("should return statusCode 400 if refreshToken is not provided", async () => {
+    const { sut, requiredParamsStub } = makeSut();
+
+    jest.spyOn(requiredParamsStub, "check").mockReturnValueOnce("refreshToken");
+
+    const httpRequest = {
+      accessToken: "valid_access_token",
+    };
+
+    const res = await sut.handle({ body: httpRequest });
+
+    expect(res.statusCode).toBe(400);
+  });
+});
