15b345b91ec4207166c496d98953842fa3fe700a
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-08-21 18:55:00 -0300

    chore: update README.md to add enviroments vars doc

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,7 +2,5 @@ export default {
   mongoUrl: process.env.MONGO_URL || "mongodb://0.0.0.0:27017/auth2",
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
   port: process.env.PORT || 5050,
-  secretRefreshTokenJwt:
-    process.env.REFRESH_TOKEN_JWT || "secret_refresh_token_jwt",
   secretAccessTokenJwt: process.env.SECRET_JWT || "secret_jwt",
 };
