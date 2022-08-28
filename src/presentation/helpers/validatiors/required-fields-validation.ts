1dfcecbda1a1f723e4f8d8150acd2d37ab211901
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-08-28 14:04:32 -0300

    feat: create ValidationComposite and RequiredFieldsComposite

---
+++ src/presentation/helpers/validatiors/required-fields-validation.ts
@@ -0,0 +1,13 @@
+import { MissingParamError } from "../../errors";
+import type { Validation } from "./validation";
+
+export class RequiredFieldValidation implements Validation {
+  public constructor(private readonly fieldName: string) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public validate(input: any): Error {
+    if (!input[this.fieldName]) {
+      return new MissingParamError(this.fieldName);
+    }
+  }
+}
---
+++ src/presentation/helpers/validatiors/validation-composite.ts
@@ -0,0 +1,13 @@
+import type { Validation } from "./validation";
+
+export class ValidationComposite implements Validation {
+  public constructor(private readonly validations: Array<Validation>) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public validate(input: any): Error {
+    for (const validation of this.validations) {
+      const error = validation.validate(input);
+      if (error) return error;
+    }
+  }
+}
