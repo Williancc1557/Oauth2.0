834c1346d97e15b5c92edcc41114350e5f4e48ea
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-08-28 16:12:17 -0300

    test: create the email validation

---
+++ src/presentation/helpers/validatiors/email-validation.spec.ts
@@ -0,0 +1,50 @@
+import { InvalidParamError } from "../../errors";
+import type { ValidateEmail } from "../../protocols/validate-email";
+import { EmailValidation } from "./email-validation";
+
+const makeEmailValidator = (): ValidateEmail => {
+  class EmailValidatorStub implements ValidateEmail {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    public validate(email: string): boolean {
+      return true;
+    }
+  }
+  return new EmailValidatorStub();
+};
+
+const makeSut = () => {
+  const emailValidatorStub = makeEmailValidator();
+  const sut = new EmailValidation("email", emailValidatorStub);
+
+  return {
+    sut,
+    emailValidatorStub,
+  };
+};
+
+describe("Email Validation", () => {
+  test("should return error if email is not valid", async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, "validate").mockReturnValueOnce(false);
+    const response = sut.validate({ email: "any_email@mail.com" });
+
+    expect(response).toStrictEqual(new InvalidParamError("email"));
+  });
+
+  test("should call EmailValidator with correct email", async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const validateSpy = jest.spyOn(emailValidatorStub, "validate");
+    sut.validate({ email: "any_email@mail.com" });
+
+    expect(validateSpy).toHaveBeenCalledWith("any_email@mail.com");
+  });
+
+  test("should return 500 if EmailValidator throws", async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, "validate").mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    expect(sut.validate).toThrow();
+  });
+});
