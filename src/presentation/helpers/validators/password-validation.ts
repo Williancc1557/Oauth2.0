ea25c59905d2a5d2d8473defb7a08072651304c7
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-08-28 17:06:11 -0300

    feat: create the PasswordValidation

---
+++ src/presentation/helpers/validators/password-validation.ts
@@ -0,0 +1,20 @@
+import { InvalidParamError } from "../../errors";
+import type { PasswordValidator } from "../../protocols/password-validator";
+import type { Validation } from "./validation";
+
+export class PasswordValidation implements Validation {
+  public constructor(
+    private readonly fieldPassword: string,
+    private readonly passwordValidator: PasswordValidator
+  ) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public validate(input: any): Error {
+    const password = input[this.fieldPassword];
+    const isValidPassword = this.passwordValidator.validate(password);
+
+    if (!isValidPassword) {
+      return new InvalidParamError(this.fieldPassword);
+    }
+  }
+}
