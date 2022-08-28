027e723fb65f8524c3c4c1d1389460ead27e7089
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-08-28 16:32:43 -0300

    feat: create the NameValidation

---
+++ src/presentation/helpers/validators/name-validation.ts
@@ -0,0 +1,20 @@
+import { InvalidParamError } from "../../errors";
+import type { NameValidator } from "../../protocols/name-validator";
+import type { Validation } from "./validation";
+
+export class NameValidation implements Validation {
+  public constructor(
+    private readonly fieldName: string,
+    private readonly nameValidator: NameValidator
+  ) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public validate(input: any): Error {
+    const name = input[this.fieldName];
+    const isValidName = this.nameValidator.validate(name);
+
+    if (!isValidName) {
+      return new InvalidParamError(this.fieldName);
+    }
+  }
+}
