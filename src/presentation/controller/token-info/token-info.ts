db7dab3fecaeb4d5df87f298f0cf3a37f29e75d4
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-08-28 12:39:07 -0300

    feat: ensure TokenInfoController call validation with valid values

--- src/presentation/controller/token-info/token-info.ts
+++ src/presentation/controller/token-info/token-info.ts
@@ -5,6 +5,7 @@ import {
   serverError,
   unauthorized,
 } from "../../helpers/http-helper";
+import type { Validation } from "../../helpers/validations/validation";
 import type {
   Controller,
   HttpRequest,
@@ -18,11 +19,13 @@ export class TokenInfoController implements Controller {
   public constructor(
     private readonly requiredParams: RequiredParams,
     private readonly getTokenInfo: GetTokenInfo,
-    private readonly verifyAccessToken: VerifyAccessToken
+    private readonly verifyAccessToken: VerifyAccessToken,
+    private readonly validation: Validation
   ) {}
 
   public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.header);
       const requiredParam = this.requiredParams.check(
         ["accesstoken"],
         httpRequest.header
