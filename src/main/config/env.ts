69a4d8d1f6aea4c714e1a47e574225d519e47c6f
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-09-11 21:18:16 -0300

    refactor: change port number on env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
   mongoUrl: process.env.MONGO_URL || "mongodb://0.0.0.0:27017/auth2",
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-  port: process.env.PORT || 5050,
+  port: process.env.PORT || 8080,
   secretAccessTokenJwt: process.env.SECRET_JWT || "secret_jwt",
 };
