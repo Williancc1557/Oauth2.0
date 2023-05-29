3efd5c05305499ff537616cbbe0d8872bcb3234e
Willian Cavalcanti Coelho <willian.cavalcanti@jala.university>
2023-05-29 18:39:17 -0300

    chore: ensure env is working

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,7 @@
+import { config } from "dotenv";
+
+config();
+
 export default {
   mongoUrl: process.env.MONGO_URL || "mongodb://0.0.0.0:27017/auth2",
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
