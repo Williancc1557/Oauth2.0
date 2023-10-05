97cd64fa72b9dc62c275ace47ff15a8110726b55
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2023-10-05 11:56:34 -0300

    fix: options are not allowed

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -4,5 +4,12 @@ export const cors = (req: Request, res: Response, next: NextFunction): void => {
   res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Headers", "*");
   res.set("Access-Control-Allow-Methods", "*");
+
+  if (req.method == "OPTIONS") {
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+    res.sendStatus(200);
+    return;
+  }
+
   next();
 };
