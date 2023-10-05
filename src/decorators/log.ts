438d51cb3813354c79c3a8e7dd4113c4321b6d38
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2023-10-05 19:06:25 -0300

    chore: implment logs

--- src/decorators/log.ts
+++ src/decorators/log.ts
@@ -1,3 +1,4 @@
+import { mongoHelper } from "../infra/db/mongodb/helpers/mongo-helper";
 import type {
   Controller,
   HttpRequest,
@@ -10,25 +11,35 @@ export class LogControllerDecorator implements Controller {
 
   public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     logger.info("Router executed");
-    logger.info(`bodyRequest: ${JSON.stringify(httpRequest.body)}`);
 
     const controller = await this.controller.handle(httpRequest);
 
-    const bodyResponse = `bodyResponse: ${JSON.stringify(controller.body)}`;
+    const bodyResponse = `error: ${JSON.stringify(controller.body)}`;
 
     const OK = 200;
     const MULTIPLE_CHOICES = 300;
     const INTERNAL_SERVER_ERROR = 500;
+    const logsCollection = await mongoHelper.getCollection("logs");
+
     if (
       controller.statusCode >= OK &&
       controller.statusCode < MULTIPLE_CHOICES
     ) {
-      logger.info(bodyResponse);
+      logger.info("Router execute with success", controller.statusCode);
     } else if (controller.statusCode == INTERNAL_SERVER_ERROR) {
       logger.fatal(bodyResponse);
     } else {
       logger.warn(bodyResponse);
     }
+
+    await logsCollection.insertOne({
+      createdAt: new Date(),
+      statusCode: controller.statusCode,
+      origin: httpRequest.header.origin,
+      body: controller.body.error,
+      userAgent: httpRequest.header["user-agent"],
+      userSystemOperation: httpRequest.header["sec-ch-ua-platform"],
+    });
 
     return controller;
   }
