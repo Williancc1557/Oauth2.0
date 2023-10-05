980a89e47e6eb87635279623c0b01f88a6e8679a
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2023-10-05 09:32:03 -0300

    fix: build is not working

--- build/main/middlewares/content-type.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.contentType = void 0;
-const contentType = (req, res, next) => {
-    res.setHeader("Content-Type", "application/json");
-    next();
-};
-exports.contentType = contentType;
--- build/main/routes/auth/refresh-token-router/refresh-token-router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_route_adapter_1 = require("../../../adapters/express-route-adapter");
-const refresh_token_1 = require("../../../factories/refresh-token/refresh-token");
-exports.default = (router) => {
-    router.get("/auth/refresh-token", (0, express_route_adapter_1.adaptRoute)((0, refresh_token_1.makeRefreshTokenController)()));
-};
