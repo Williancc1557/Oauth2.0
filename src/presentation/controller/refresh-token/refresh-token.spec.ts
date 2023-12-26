1304faa39e9590b422c3303f5f4c895d213a0188
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2023-12-25 21:50:00 -0300

    refactor: optimize refresh-token tests

--- src/presentation/controller/refresh-token/refresh-token.spec.ts
+++ src/presentation/controller/refresh-token/refresh-token.spec.ts
@@ -1,9 +1,4 @@
 import { RefreshTokenController } from "./refresh-token";
-import type {
-  CreateAccessToken,
-  CreateAccessTokenOutput,
-} from "../../../data/protocols/create-access-token";
-import type { CheckRefreshToken } from "../../../domain/usecase/check-refresh-token";
 import {
   badRequest,
   ok,
@@ -11,7 +6,6 @@ import {
   unauthorized,
 } from "../../helpers/http-helper";
 import { InvalidParamError } from "../../errors";
-import type { Validation } from "../../protocols/validation";
 
 const makeFakeHttpRequest = () => ({
   header: {
@@ -19,46 +13,19 @@ const makeFakeHttpRequest = () => ({
   },
 });
 
-const makeCheckRefreshTokenStub = (): CheckRefreshToken => {
-  class CheckRefreshToken implements CheckRefreshToken {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    public async check(refreshToken: string): Promise<string | undefined> {
-      return "valid_user_id";
-    }
-  }
-
-  return new CheckRefreshToken();
-};
-
-const makeCreateAccessTokenStub = (): CreateAccessToken => {
-  class CreateAccessToken implements CreateAccessToken {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    public create(userId: string): CreateAccessTokenOutput {
-      return {
-        accessToken: "valid_access_token",
-        expiresIn: 300,
-      };
-    }
-  }
-
-  return new CreateAccessToken();
-};
-
-const makeValidationStub = (): Validation => {
-  class ValidationStub implements Validation {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    public validate(input: any): Error {
-      return null;
-    }
-  }
-
-  return new ValidationStub();
-};
-
 const makeSut = () => {
-  const checkRefreshTokenStub = makeCheckRefreshTokenStub();
-  const createAccessTokenStub = makeCreateAccessTokenStub();
-  const validationStub = makeValidationStub();
+  const createAccessTokenStub = {
+    create: jest.fn(() => ({
+      accessToken: "valid_access_token",
+      expiresIn: 300,
+    })),
+  };
+  const checkRefreshTokenStub = {
+    check: jest.fn(async () => "valid_user_id"),
+  };
+  const validationStub = {
+    validate: jest.fn(),
+  };
 
   const sut = new RefreshTokenController(
     checkRefreshTokenStub,
@@ -77,10 +44,7 @@ const makeSut = () => {
 describe("RefreshToken Controller", () => {
   test("should return 500 if checkRefreshToken has any error", async () => {
     const { sut, checkRefreshTokenStub } = makeSut();
-
-    jest
-      .spyOn(checkRefreshTokenStub, "check")
-      .mockRejectedValueOnce(new Error());
+    checkRefreshTokenStub.check.mockRejectedValueOnce(new Error());
 
     const httpResponse = await sut.handle(makeFakeHttpRequest());
 
@@ -90,7 +54,7 @@ describe("RefreshToken Controller", () => {
   test("should return 500 if createAccessTokenStub has any error", async () => {
     const { sut, createAccessTokenStub } = makeSut();
 
-    jest.spyOn(createAccessTokenStub, "create").mockImplementation(() => {
+    createAccessTokenStub.create.mockImplementation(() => {
       throw new Error();
     });
 
@@ -101,7 +65,7 @@ describe("RefreshToken Controller", () => {
 
   test("should return 400 if checkRefreshToken returns null", async () => {
     const { sut, checkRefreshTokenStub } = makeSut();
-    jest.spyOn(checkRefreshTokenStub, "check").mockReturnValueOnce(null);
+    checkRefreshTokenStub.check.mockReturnValueOnce(null);
     const httpResponse = await sut.handle(makeFakeHttpRequest());
 
     expect(httpResponse).toStrictEqual(unauthorized());
@@ -109,7 +73,7 @@ describe("RefreshToken Controller", () => {
 
   test("should checkRefreshToken is called with correct accessToken", async () => {
     const { sut, checkRefreshTokenStub } = makeSut();
-    const checkRefreshTokenSpy = jest.spyOn(checkRefreshTokenStub, "check");
+    const checkRefreshTokenSpy = checkRefreshTokenStub.check;
     await sut.handle(makeFakeHttpRequest());
 
     expect(checkRefreshTokenSpy).toBeCalledWith("valid_access_token");
@@ -117,7 +81,7 @@ describe("RefreshToken Controller", () => {
 
   test("should create access is called with correct id", async () => {
     const { sut, createAccessTokenStub } = makeSut();
-    const createAccessTokenSpy = jest.spyOn(createAccessTokenStub, "create");
+    const createAccessTokenSpy = createAccessTokenStub.create;
     await sut.handle(makeFakeHttpRequest());
 
     expect(createAccessTokenSpy).toBeCalledWith("valid_user_id");
@@ -138,9 +102,9 @@ describe("RefreshToken Controller", () => {
   test("should return 400 if validation returns an error", async () => {
     const { sut, validationStub } = makeSut();
 
-    jest
-      .spyOn(validationStub, "validate")
-      .mockReturnValueOnce(new InvalidParamError("refreshtoken"));
+    validationStub.validate.mockReturnValueOnce(
+      new InvalidParamError("refreshtoken")
+    );
 
     const httpResponse = await sut.handle(makeFakeHttpRequest());
 
