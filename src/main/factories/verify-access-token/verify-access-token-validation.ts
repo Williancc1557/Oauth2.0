a4258baa183400ea9acd634def1a9e19fffbb5d3
willian <willian.cacoelho@gmail.com>
2024-04-22 18:48:45 -0300

    feat: ensure verify access token validation is working

---
+++ src/main/factories/verify-access-token/verify-access-token-validation.ts
@@ -0,0 +1,13 @@
+import { RequiredFieldValidation } from "../../../presentation/helpers/validators/required-field-validation";
+import { ValidationComposite } from "../../../presentation/helpers/validators/validation-composite";
+import type { Validation } from "../../../presentation/protocols/validation";
+
+export const makeAccessTokenValidation = () => {
+  const validations: Array<Validation> = [];
+
+  for (const field of ["authorization"]) {
+    validations.push(new RequiredFieldValidation(field));
+  }
+
+  return new ValidationComposite(validations);
+};
