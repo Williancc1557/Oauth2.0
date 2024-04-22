b67a6544880707c578077454af3f2e72e9a15c35
willian <willian.cacoelho@gmail.com>
2024-04-22 18:32:10 -0300

    feat: ensure verify access token controller returns badRequest if authorization was not provided

--- src/presentation/controller/verify-access-token.ts
+++ src/presentation/controller/verify-access-token.ts
@@ -1,14 +1,19 @@
-import { ok, serverError } from "../helpers/http-helper";
+import { badRequest, ok, serverError } from "../helpers/http-helper";
 import type { Controller, HttpRequest, HttpResponse } from "../protocols";
+import type { Validation } from "../protocols/validation";
 import type { VerifyAccessToken } from "../protocols/verify-access-token";
 
 export class VerifyAccessTokenController implements Controller {
   public constructor(
-    private readonly utilVerifyAccessToken: VerifyAccessToken
+    private readonly utilVerifyAccessToken: VerifyAccessToken,
+    private readonly validation: Validation
   ) {}
 
   public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const error = this.validation.validate(httpRequest.header);
+      if (error) return badRequest(error);
+
       const isValidToken = this.utilVerifyAccessToken.verify(
         httpRequest.header.authorization
       );
